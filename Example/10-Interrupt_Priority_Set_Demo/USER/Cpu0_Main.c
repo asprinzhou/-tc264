003eb9e607560f68b4329c4838de6d3cc434e760
SEEKFREE_Kang <kang.xu@seekfree.cn>
2020-04-12 21:16:13 +0800

    V1.0 初始版本

---
+++ Example/10-Interrupt_Priority_Set_Demo/USER/Cpu0_Main.c
@@ -0,0 +1,48 @@
+/*********************************************************************************************************************
+ * COPYRIGHT NOTICE
+ * Copyright (c) 2020,逐飞科技
+ * All rights reserved.
+ * 技术讨论QQ群：三群：824575535
+ *
+ * 以下所有内容版权均属逐飞科技所有，未经允许不得用于商业用途，
+ * 欢迎各位使用并传播本程序，修改内容时必须保留逐飞科技的版权声明。
+ *
+ * @file       		main
+ * @company	   		成都逐飞科技有限公司
+ * @author     		逐飞科技(QQ3184284598)
+ * @version    		查看doc内version文件 版本说明
+ * @Software 		tasking v6.3r1
+ * @Target core		TC264D
+ * @Taobao   		https://seekfree.taobao.com/
+ * @date       		2020-3-23
+ ********************************************************************************************************************/
+
+
+#include "headfile.h"
+
+uint16 adc_result;
+
+int core0_main(void)
+{
+	disableInterrupts();
+	get_clk();//获取时钟频率  务必保留
+
+    //本例程没有代码
+
+	//中断优先级都在isr_config.h中 进行设置
+
+	//以及中断的服务者也在isr_config.h中 进行设置
+
+	//所有的中断  优先级务必保证不一样，不能有重复的优先级
+
+	//在uart、pit、GPIO中断例程中的备注都有提到
+
+
+    enableInterrupts();
+
+    while (TRUE)
+    {
+
+    }
+}
+
